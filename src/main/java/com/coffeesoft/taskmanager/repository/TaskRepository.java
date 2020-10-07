852806424e15d12afda0589b3d5bf871656ecef1
Maxuha <zykovmaksym@gmail.com>
2020-10-07 13:38:59 +0300

    Added new method for find all tasks for user

--- src/main/java/com/coffeesoft/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/coffeesoft/taskmanager/repository/TaskRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
     @Query(value = "SELECT * FROM task WHERE time > ?1 AND user_id = ?2 ORDER BY time DESC LIMIT 1",
     nativeQuery = true)
     Optional<Task> findNextOrCurrentTaskAfterTimeByUserId(LocalDateTime time, Long userId);
+
+    List<Task> findByUserId(Long userId);
 }
