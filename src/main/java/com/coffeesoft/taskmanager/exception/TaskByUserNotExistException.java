387a5c969070837fc55e81aa1691b462f561600e
Maxuha <zykovmaksym@gmail.com>
2020-10-07 13:21:41 +0300

    Added new exception for happening when current or next tasks for user not exist

---
+++ src/main/java/com/coffeesoft/taskmanager/exception/TaskByUserNotExistException.java
@@ -0,0 +1,11 @@
+package com.coffeesoft.taskmanager.exception;
+
+public class TaskByUserNotExistException extends NotExistException {
+    public TaskByUserNotExistException(Long userId) {
+        super(String.format("Current or next task for user with %d not exist", userId));
+    }
+
+    public TaskByUserNotExistException(Long userId, Throwable cause) {
+        super(String.format("Current or next task for user with %d not exist", userId), cause);
+    }
+}
