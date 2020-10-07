04d19dd6da87c051f44b0b98b2c161844ab72500
Maxuha <zykovmaksym@gmail.com>
2020-10-07 10:51:30 +0300

    Added method for getting next or current running task

--- src/main/java/com/coffeesoft/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/coffeesoft/taskmanager/repository/TaskRepository.java
@@ -2,6 +2,10 @@ package com.coffeesoft.taskmanager.repository;
 
 import com.coffeesoft.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    @Query(value = "SELECT * FROM task WHERE time > current_timestamp ORDER BY time DESC LIMIT 1",
+    nativeQuery = true)
+    Task findNextOrCurrentTaskByUserId(Long userId);
 }
